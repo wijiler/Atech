f2c1c69281748c3ae3dd5974ad245ba53fea5b23
wij <41879253+wijiler@users.noreply.github.com>
2021-02-27 22:15:29 +0000

    Unfinished source code

---
+++ Src/lexer.c
@@ -0,0 +1,37 @@
+#include "include/lexer.h"
+ #include <stdlib.h>
+
+lexer_T* init_lexer(char* contents)
+{
+    lexer_T* lexer = calloc(1, sizeof(struct LEXER_STRUCT));
+    lexer->contents = contents;
+    lexer->i = 0;
+    lexer->c = contents[lexer->i];
+
+    return lexer;
+}
+
+void lexer_advance(lexer_T* lexer)
+{
+
+}
+
+void lexer_skip_whitespace(lexer_T* lexer)
+{
+
+}
+
+token_T* lexer_get_next_token(lexer_T* lexer)
+{
+
+}
+
+token_T* lexer_collect_string(lexer_T* lexer)
+{
+
+}
+
+char* lexer_get_current_char_as_string(lexer_T* lexer)
+{
+
+}
